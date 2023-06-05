cde17ebd73083c56e445da33e56d1ea12c3c53b5
seaboyz <seaboyz@hotmail.com>
2023-06-04 20:15:43 -0500

    test initial condition

--- index.test.js
+++ index.test.js
@@ -1,32 +1,21 @@
 const { expect } = require('chai');
-const { alwaysTrue, legitString } = require('./index.js');
+const _ = require('lodash');
+const { getPerson } = require('./index.js');
 
-describe('#mocha basics', () => {
-  it('true should be true', () => {
-    expect(true).to.be.true;
+describe('#basic mocha chai', () => {
+  it('should be true', () => {
+    expect(true).to.true;
   });
 });
 
-describe('#alwaysTrue', () => {
-  it('always true should return true', () => {
-    // act
-    const result = alwaysTrue();
-    // assert
-    expect(result).to.be.true;
+describe('#index initial conditions', () => {
+  it('inital person is an object', () => {
+    const person = getPerson();
+    expect(_.isObject(person)).to.be.true;
   });
 
-  it('should not be false', () => {
-    const result = alwaysTrue();
-    expect(result).not.to.be.false;
-  });
-});
-
-describe('#legitString', () => {
-  it('should detect "cow" be a legit string', () => {
-    expect(legitString('cow')).to.be.true;
-  });
-
-  it('undefined should not be true', () => {
-    expect(legitString(undefined)).to.be.false;
+  it.only('armorBonus by default is 2 wearing leatherArmor', () => {
+    const person = getPerson();
+    expect(person.armorBonus).to.equal(2);
   });
 });
